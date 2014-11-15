7e090b0fa6a060123b7f2fee0080b665a6478625
Mynul Islam <saaifulislam@gmail.com>
2014-11-15 16:57:56 +0600

    Fix Some Problem

--- restore.bat
+++ restore.bat
@@ -1,12 +1,14 @@
 @echo off
-SET var=%cd%
+SET var=H:\MYNUL COLLECTION\TEXT\imp\Dropbox\Dropbox\st3
+RD "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text 3\Installed Packages\" /S /Q
+RD "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text 3\Packages\" /S /Q
 echo ===============================================
 echo =  SUBLIME TEXT 3 Packages Backup to Dropbox  =
 echo ===============================================
 echo Restoring Installed Packages....
-XCOPY "%cd%\Installed Packages\*" "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text 3\Installed Packages\" /s /i /h /q /y
+XCOPY "%var%\Installed Packages\*" "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text 3\Installed Packages\" /s /i /h /q /y
 echo Restoring Packages....
-XCOPY "%cd%\Packages\*" "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text 3\Packages\"  /s /i /h /q /y
+XCOPY "%var%\Packages\*" "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text 3\Packages\"  /n /r /c /s /i /h /q /y
 echo ===============================================
 echo =  	    Voila! Job Done !  		   =
 echo ===============================================
